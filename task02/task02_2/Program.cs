0ce0bef7ec4d884df586e9fe6bf87bb6324232da
lushnikov_k <lushnikov_k@bk.ru>
2019-07-16 01:18:34 +0300

    ENCAPSULATION

---
+++ task02/task02_2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+
+namespace task02_2
+{
+    class Program
+    {
+        class Triangle
+        {
+            private double A, B, C;
+            public Triangle(double a, double b, double c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+            public double a
+            {
+                get
+                {
+                    return A;
+                }
+                set
+                {
+                    if (value >= 0)
+                        A = value;
+                    else
+                        throw new ArgumentException("Данные введены неккоректно!");
+                }
+            }
+            public double b
+            {
+                get
+                {
+                    return B;
+                }
+                set
+                {
+                    if (value >= 0)
+                        B = value;
+                    else
+                        throw new ArgumentException("Данные введены неккоректно!");
+                }
+            }
+            public double c
+            {
+                get
+                {
+                    return C;
+                }
+                set
+                {
+                    if (value >= 0)
+                        C = value;
+                    else
+                        throw new ArgumentException("Данные введены неккоректно!");
+                }
+            }
+            public double Perimetr
+            {
+                get
+                {
+                    return a + b + c;
+                }
+            }
+            public double Area
+            {
+                get
+                {
+                    double SemiPerimetr = Perimetr * 0.5;
+                    return Math.Sqrt(SemiPerimetr * (SemiPerimetr - a) * (SemiPerimetr - b) * (SemiPerimetr - c));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("введите сторону a");
+            //double x = double.Parse(Console.ReadLine());
+            if (!double.TryParse(Console.ReadLine(), out double a))
+            {
+                throw new ArgumentException("Данные введены неккоректно!");
+            }
+            Console.WriteLine("введите сторону b");
+            if (!double.TryParse(Console.ReadLine(), out double b))
+            {
+                throw new ArgumentException("Данные введены неккоректно!");
+            }
+            Console.WriteLine("Введите сторону с ");
+            if (!double.TryParse(Console.ReadLine(), out double c))
+            {
+                throw new ArgumentException("Данные введены неккоректно!");
+            }
+            Triangle triangle = new Triangle(a, b, c);
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                Console.WriteLine("Периметр равен: " + triangle.Perimetr);
+                Console.WriteLine("Площадь равна: " + triangle.Area);
+            }
+            else
+            {
+                throw new ArgumentException("Данные введены неккоректно!");
+            }
+               
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task02/task02_3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace task02_3
+{
+    class Program
+    {
+        class User
+        {
+            private string Firstname,Lastname,Patronymic;
+
+            public User(string firstname, string lastname, string patronymic, DateTime birthday)
+            {
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.patronymic = patronymic;
+                this.birthday = birthday;
+            }
+            public string firstname
+            {
+                get
+                {
+                    return Firstname;
+                }
+                set
+                {
+                    if (value != "")
+                        Firstname = value;
+                    else
+                        throw new ArgumentException("Данные введены неккоректно!");
+                }
+            }
+            public string lastname
+            {
+                get
+                {
+                    return Lastname;
+                }
+                set
+                {
+                    if (value != "")
+                        Lastname = value;
+                    else
+                        throw new ArgumentException("Данные введены неккоректно!");
+                }
+            }
+            public string patronymic
+            {
+                get
+                {
+                    return Patronymic;
+                }
+                set
+                {
+                    if (value != "")
+                        Patronymic = value;
+                    else
+                        throw new ArgumentException("Данные введены неккоректно!");
+                }
+            }
+            public DateTime birthday { get; set; }
+            public int age
+            {
+                get
+                {
+                    return DateTime.Now.Year - birthday.Year;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("введите имя");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("введите фамилию");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Введите Отчество");
+            string patronymic = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+            if (!DateTime.TryParse(Console.ReadLine(), out DateTime birthdate))
+            {
+                throw new ArgumentException("Данные введены неккоректно!");
+            }
+            User user = new User(firstname, lastname, patronymic, birthdate);
+            Console.WriteLine("{0}, {1}, {2}, {3}, Дата рождения  {4}",  user.firstname, user.lastname,user.patronymic,user.age, user.birthday);
+            Console.ReadKey();
+        }
+    }
+}
