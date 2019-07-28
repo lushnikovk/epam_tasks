c5e74720d06019d9fedbfb2cd2bf452c70bb8a82
lushnikov_k <lushnikov_k@bk.ru>
2019-07-28 18:16:22 +0300

    CUSTOM SORT AND CUSTOM SORT DEMO DONE

---
+++ task04/task04_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a;
+            Input(out a);
+            Console.WriteLine("Массив до сортировки");
+            Print(a);
+            Console.WriteLine();
+            Sort(a, (n, m) => n> m);
+            Console.WriteLine("Массив после сортировки");
+            Print(a);
+            Console.ReadKey();
+        }
+        public static void Sort<T>(T[] a, Func<T, T, bool> compare)
+        {
+
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                for (int j = 0; j < a.Length-1; j++)
+                {
+                    if (compare(a[j], a[j+1]))
+                    {
+                        T temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+
+            
+        }
+        static void Input(out int[] a)
+        {
+            int r = 10;
+            a = new int[r];
+
+            Random rnd = new Random();
+            for (int i = 0; i < r; i++)
+            {
+                {
+                    a[i] = rnd.Next(0, 10);
+                }
+
+            }
+        }
+        static void Print(int[] a)
+        {
+
+            foreach (var item in a)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ task04/task04_2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new[] { "b","bb","a","beagle","cin","can","cout" };
+           // Input(out a);
+            Console.WriteLine("Массив до сортировки");
+            Print(a);
+            Console.WriteLine();
+            Sort(a, Compare);
+            Console.WriteLine("Массив после сортировки");
+            Print(a);
+            Console.ReadKey();
+        }
+        public static void Sort<T>(T[] array, Func<T, T, bool> comparer)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (comparer(array[j], array[j + 1]))
+                    {
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+
+        }
+        static bool Compare(string str1, string str2)
+        {
+            if (str1.Length > str2.Length)
+            {
+                return true;
+            }
+
+            if (str1.Length == str2.Length)
+            {
+                if (str1[0] < str2[0]) return false;
+
+                for (int i = 1; i < str1.Length; i++)
+                {
+                    if (str1[i] > str2[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (str1.Length == 1 && str2.Length == 1)
+                return true;
+
+            return false;
+        }
+        static void Print(string[] a)
+        {
+
+            foreach (var item in a)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
